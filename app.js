b6534a1ad9f98a7e35d957a41ee98ebf83c23018
Shannon Archer <shannon.archer@zoro.com>
2018-03-06 23:37:07 -0600

    updated app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'zpp.html'));
+  res.sendFile(path.join(__dirname, 'zpp.html'));
 });
 
 app.listen(5020);
